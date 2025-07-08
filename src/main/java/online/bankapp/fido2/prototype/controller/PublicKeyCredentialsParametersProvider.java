815eb211c1d521551d8fd69e8da5909b95a49f42
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-07-08 18:50:31 +0200

    refactor(auth): update public key credential parameters to support multiple algorithms

--- src/main/java/online/bankapp/fido2/prototype/controller/PublicKeyCredentialsParametersProvider.java
+++ src/main/java/online/bankapp/fido2/prototype/controller/PublicKeyCredentialsParametersProvider.java
@@ -4,12 +4,21 @@ import com.webauthn4j.data.PublicKeyCredentialParameters;
 import com.webauthn4j.data.PublicKeyCredentialType;
 import com.webauthn4j.data.attestation.statement.COSEAlgorithmIdentifier;
 
-public final class PublicKeyCredentialsParametersProvider {
-    public static PublicKeyCredentialParameters getPubKeyCredParam() {
+import java.util.List;
 
-        return new PublicKeyCredentialParameters(
+public final class PublicKeyCredentialsParametersProvider {
+    public static List<PublicKeyCredentialParameters> getPubKeyCredParamList() {
+
+        var keyParam1 = new PublicKeyCredentialParameters(
                 PublicKeyCredentialType.PUBLIC_KEY,
                 COSEAlgorithmIdentifier.ES256
         );
+
+        var keyParam2 = new PublicKeyCredentialParameters(
+                PublicKeyCredentialType.PUBLIC_KEY,
+                COSEAlgorithmIdentifier.RS256
+        );
+
+        return List.of(keyParam1, keyParam2);
     }
 }
